4707da7d5e382c634e710eaba1d3f0deb41c7ad0
Meiling-Luo <114460427+Meiling-Luo@users.noreply.github.com>
2023-06-08 15:00:39 -0400

    Update Registration.cshtml.cs

--- Pages/Registration.cshtml.cs
+++ Pages/Registration.cshtml.cs
@@ -1,67 +1,80 @@
 using AcademicManagement;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace Lab2.Pages
 {
     public class RegistrationModel : PageModel
     {
-        [BindProperty]
-        public string? SelectedStudentName { get; set; }
-
-        public List<Course>? AllCourses { get; set; }
-
-        [BindProperty]
-        public List<string> SelectedCourseCode { get; set; } = new List<string>();
-
 
         [BindProperty]
 
-        public Course? SelectedCourse { get; set; }
+
+        public List<AcademicRecord> academicRecords { get; set; } = new List<AcademicRecord>();
 
         [BindProperty]
-
-        public bool ShowEmptyRegistrationError { get; set; } = false;
-        public bool ShowRegistrationInfo { get; set; } = false;
+        public string selectedStudentId { get; set; }
 
 
+        [BindProperty]
+        public List<SelectListItem>? cSelections { get; set; }
+
+        [BindProperty]
+        public List<Course> SelectedCourses { get; set; }
 
 
+        public Boolean NameCheck { get; set; } = false;
+        public Boolean CourseCheck { get; set; } = false;
+        public int LoadTime { get; set; } = 0;
         public void OnGet()
         {
-
         }
 
         public void OnPost()
         {
-            if (!string.IsNullOrEmpty(SelectedStudentName))
+            LoadTime += 1;
+            if (selectedStudentId != "-1")
             {
-                ShowEmptyRegistrationError = false;
-                ShowRegistrationInfo = true;
+                NameCheck = true;
+                academicRecords = DataAccess.GetAcademicRecordsByStudentId(selectedStudentId);
+
+                if (academicRecords.Count() > 0)
+                {
+                    NameCheck = true;
+                    CourseCheck = true;
+
+                    SelectedCourses = new List<Course>();
+                    foreach (AcademicRecord ar in academicRecords)
+                    {
+                        SelectedCourses.Add(DataAccess.GetAllCourses().First(c => c.CourseCode == ar.CourseCode));
+                    }
+
+                }
             }
-            else
-            {
-                ShowEmptyRegistrationError = true;
-                ShowRegistrationInfo = false;
-
-
-            }
-
-            if (SelectedCourseCode != null)
-            {
-                SelectedCourse = DataAccess.GetAllCourses().FirstOrDefault(c => SelectedCourseCode.Contains(c.CourseCode));
-
-
-            }
-
-            else
-            {
-                SelectedCourse = null;
-            }
-
 
 
         }
+
+        public void OnPostRegister()
+        {
+
+            NameCheck = true;
+
+            foreach (SelectListItem item in cSelections)
+            {
+                if (item.Selected)
+                {
+                    CourseCheck = true;
+                    SelectedCourses.Add(DataAccess.GetAllCourses().First(c => c.CourseCode == item.Value));
+                    DataAccess.AddAcademicRecord(new AcademicRecord(selectedStudentId, item.Value));
+
+                }
+            }
+
+        }
+
 
     }
 }
